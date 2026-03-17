10a540bccfbcf2c3b64e08b0c1fcd5e0f186bfb3
Murilorufino15 <murilorufino89@gmail.comgit config --global user.email murilorufino89@gmail.comgit config --global user.email murilorufino89@gmail.com>
2026-03-17 15:24:43 -0300

    :tada: init: Primeiro commit realizado.

---
+++ script.js
@@ -0,0 +1,56 @@
+const idade = 30 // caixinha idade recebe o valor de 30.
+let nome = "Murilo";
+var cidade = "Sao Caetano Do Sul";
+
+console.log(idade);
+console.log(nome);
+console.log(cidade);
+/*
+console.log(nome);
+nome ="Lucas";
+console.log(nome);
+
+console.log(idade);
+idade = "15";
+console.log(idade);
+*/
+
+/*
+let mensagem = "Olá mundo!"
+console.log(typeof mensagem);
+
+console.log(`Meu nome é ${nome}, tenho ${idade} anos de idade`)
+console.log("juntando nome " +nome + "com idade " +idade )
+
+let quantidade = 5
+let temperatura = 36.5
+console.log(typeof quantidade)
+console.log(`Eu tenho ${quantidade} anos, e estou com ${temperatura} graus de febre `)
+
+let estaChovendo = true
+let temSol = false
+console.log(typeof temSol)
+
+let pedido
+console.log(typeof pedido)
+
+let endereco = null
+console.log(typeof endereco)
+*/
+
+console.log( 10 > 5) // true
+console.log( 10 <5)  // false
+
+console.log(5 == "5")// ignora o tipo (true)
+console.log(5 === "5")// verifica o tipo (false)
+
+let a = 10
+let b = 5
+console.log( a + b ) // soma
+console.log( a - b ) // subtracao
+console.log( a * b ) // multiplicacao
+console.log( a / b ) // divisao
+
+console.log( 10 % 3 ) // resto 1 
+
+console.log( 2 ** 3 ) // 2 elevado a 3 
